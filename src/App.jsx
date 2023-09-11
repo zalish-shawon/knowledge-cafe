60af3536e2327d46589cb5409bfb6727db1a910e
zalish-shawon <zalish.shawon@gmail.com>
2023-09-11 21:52:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './components/blogs/Blogs'
+import Header from './components/header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header></Header> 
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
